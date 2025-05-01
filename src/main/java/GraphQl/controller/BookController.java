1127d144cc2d41d10562a40570d1efd45ce9f61f
ohmprakashganesh <131688194+ohmprakashganesh@users.noreply.github.com>
2025-05-02 00:00:23 +0545

    some get and post

--- src/main/java/GraphQl/controller/BookController.java
+++ src/main/java/GraphQl/controller/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.http.HttpStatus;
@@ -14,12 +15,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Data;
+
 import GraphQl.entities.Book;
 import GraphQl.services.BookService;
 import lombok.AllArgsConstructor;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import GraphQl.entities.Book;
 
 
 // @RestController
@@ -53,11 +58,33 @@ public class BookController {
         
     }
 
-    @PostMapping
-    public ResponseEntity<Book> postBook(@RequestBody Book entity) {
-        return new ResponseEntity<>(bookService.create(entity), HttpStatus.CREATED);
+    @MutationMapping("createBook")
+    public Book postBook(@Argument("book") BookInput input) {
+        Book book1= new Book();
+        book1.setAuthor(input.getAuthor());
+        book1.setTitle(input.getTitle());
+        book1.setPrice(input.getPrice());
+        book1.setDesc(input.getDesc());
+        book1.setPrice(input.getPrice());
+        return bookService.create(book1);
     }
     
+
+
+
+
+
+@Data
+public class BookInput {
+    private String title;
+    private String desc;
+    private String author;
+    private double price;
+    private int pages;
+    
+
+
+}
 
     
 }
